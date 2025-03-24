25f96c9821af4272d488db8854491420edec36b3
Wiliam Rosa <wrosa85@gmail.com>
2025-03-24 11:53:53 -0300

    debug 9

--- .github/workflows/startup.sh
+++ .github/workflows/startup.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# Caminho para o ambiente virtual
+VENV_DIR="/home/site/wwwroot/antenv"
+
 # Criar ambiente virtual se não existir
-if [ ! -d "antenv" ]; then
-    python -m venv antenv
+if [ ! -d "$VENV_DIR" ]; then
+    python -m venv $VENV_DIR
 fi
 
 # Ativar o ambiente virtual
-source antenv/bin/activate
+source $VENV_DIR/bin/activate
 
-# Instalar as dependências
+# Atualizar pip
+pip install --upgrade pip
+
+# Instalar dependências
 pip install -r requirements.txt
 
 # Rodar a aplicação FastAPI
